ea67fcbfc1eeaf4c66918a62af606fc30f9ee7fd
weigangqiu <weigangqiu@rongxintx.com>
2016-12-25 13:03:53 +0800

    修复页面跳转的问题

--- consumer/src/main/java/in/colorlfire/config/SecurityConfig.java
+++ consumer/src/main/java/in/colorlfire/config/SecurityConfig.java
@@ -1,14 +1,13 @@
 package in.colorlfire.config;
 
 import in.colorlfire.serivce.CustomUserDetailsService;
-import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
 
 /**
  * @author colorfire on 16/12/10
@@ -18,16 +17,25 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+  @Override
   protected void configure(AuthenticationManagerBuilder auth) throws Exception {
     auth.userDetailsService(new CustomUserDetailsService());
   }
 
+  @Override
   protected void configure(HttpSecurity http) throws Exception {
     http.authorizeRequests()
             .antMatchers("/", "/index").permitAll()
             .anyRequest().authenticated()
-            .and().formLogin().loginPage("/login").defaultSuccessUrl("/user").permitAll()
+            .and().formLogin().loginPage("/login").failureUrl("/login-error").defaultSuccessUrl("/user").permitAll()
             .and().logout().permitAll();
+  }
+
+  @Autowired
+  public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+    auth
+            .inMemoryAuthentication()
+            .withUser("user").password("password").roles("USER");
   }
 
 }
